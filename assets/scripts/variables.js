87180051a5c89b581312ca5256c03d201ba22d62
AntResh <51818949+AntResh@users.noreply.github.com>
2019-07-13 16:52:18 +0200

    Add files via upload

---
+++ assets/scripts/variables.js
@@ -0,0 +1,58 @@
+/////////////////
+// Game status //
+/////////////////
+/**
+ * Keep save the state of the game (started or not)
+ * @type {boolean}
+ */
+var isGameStarted = true;
+
+/**
+ * Keep the index of the current player.
+ * @type {number}
+ */
+var currentPlayer = 0;
+
+/**
+ * Keep the number of click done on the grid.
+ * @type {number}
+ */
+var clickCounter = 0;
+
+/**
+ * Restart button
+ * @type {HTMLButtonElement}
+ */
+var $restartButton = document.querySelector('#restart-button');
+
+
+/////////////////
+//   Players   //
+/////////////////
+/**
+ * List of input use to set players' name.
+ * @type {NodeListOf<HTMLInputElement>}
+ */
+var $playerInputs = document.querySelectorAll('.form-control');
+
+/**
+ * Span use to display the current player's name.
+ * @type {HTMLSpanElement}
+ */
+var $currentPlayerName = document.querySelector('#player-name-display');
+
+/**
+ * List of css classes use in the grid to display crosses and circles.
+ * Indexed by players' index.
+ * @type {string[]}
+ */
+var playerClasses = ['square-text-blue', 'square-text-red'];
+
+/////////////////
+//    Grid     //
+/////////////////
+/**
+ * List of div square.
+ * @type {NodeListOf<HTMLDivElement>}
+ */
+var $squares = document.querySelectorAll('.square');
